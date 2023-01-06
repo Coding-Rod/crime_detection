5a3e7b7a334dbacc3cc51b16b0377ffcaee20647
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-06 12:08:33 -0400

    Correct topbar p1

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -8,15 +8,7 @@
         <CIcon :icon="logo" height="48" alt="Logo" />
       </CHeaderBrand>
       <CHeaderNav class="d-none d-md-flex me-auto">
-        <CNavItem>
-          <CNavLink href="/dashboard"> Dashboard </CNavLink>
-        </CNavItem>
-        <CNavItem>
-          <CNavLink href="#">Users</CNavLink>
-        </CNavItem>
-        <CNavItem>
-          <CNavLink href="#">Settings</CNavLink>
-        </CNavItem>
+        <AppBreadcrumb />
       </CHeaderNav>
       <CHeaderNav>
         <CNavItem>
@@ -24,22 +16,8 @@
             <CIcon class="mx-2" icon="cil-bell" size="lg" />
           </CNavLink>
         </CNavItem>
-        <CNavItem>
-          <CNavLink href="#">
-            <CIcon class="mx-2" icon="cil-list" size="lg" />
-          </CNavLink>
-        </CNavItem>
-        <CNavItem>
-          <CNavLink href="#">
-            <CIcon class="mx-2" icon="cil-envelope-open" size="lg" />
-          </CNavLink>
-        </CNavItem>
         <AppHeaderDropdownAccnt />
       </CHeaderNav>
-    </CContainer>
-    <CHeaderDivider />
-    <CContainer fluid>
-      <AppBreadcrumb />
     </CContainer>
   </CHeader>
 </template>
