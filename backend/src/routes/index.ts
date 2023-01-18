d8669372e89bc98cee0d792369352c497c98aca7
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-18 08:02:32 -0400

    :construction: [ADD] More routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import { Router } from 'express-serve-static-core';
 
 import usersRouter from './users.router';
+import videosRouter from './videos.router';
+import nodesRouter from './nodes.router';
 
 function routerApi(app: { use: (arg0: string, arg1: Router) => void; }) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/users', usersRouter);
+  router.use('/videos', videosRouter);
+  router.use('/nodes', nodesRouter);
 }
 
 export default routerApi;
