c894ee7c12592e18dd86462fa4603fc548ec5689
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-26 09:44:20 -0400

    :sparkles: [ADD] Home template

---
+++ frontend/src/components/CardWImage.vue
@@ -0,0 +1,73 @@
+<template>
+  <!-- FIXME: Remove space between image and margin -->
+  <CRow class="mb-4">
+    <CCol>
+      <CCard style="border-radius: 30px;">
+        <CCardBody style="padding: 0;">
+          <CRow :class="reverse ? 'card_inside_reverse' : 'card_inside'">
+            <CCol md="8" class="mt-4 mb-4 pe-4 ps-4">
+              <h2>{{ title }}</h2>
+              <p class="text-medium-emphasis">{{ subtitle }}</p>
+              <p>{{ text }}</p>
+            </CCol>
+            <CCol md="4" style="margin: 0; padding: 0; border: 0;">
+              <img :src="image" :class="reverse ? 'image_reverse' : 'image'" alt="Image" />
+            </CCol>
+          </CRow>
+        </CCardBody>
+      </CCard>
+    </CCol>
+  </CRow>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "Title",
+    },
+    subtitle: {
+      type: String,
+      default: "Subtitle",
+    },
+    image: {
+      type: String,
+      default: "https://picsum.photos/200/300",
+    },
+    text: {
+      type: String,
+      default: "Text",
+    },
+    reverse: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card_inside {
+  display: flex;
+  flex-direction: row;
+}
+.card_inside_reverse {
+  display: flex;
+  flex-direction: row-reverse;
+}
+.image {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  width: 100%;
+  border-radius: 0 30px 30px 0;
+}
+.image_reverse {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  width: 100%;
+  border-radius: 30px 0 0 30px;
+}
+</style>
