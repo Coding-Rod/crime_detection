6b903fa537710109b3973d36e25eefdbf3eb312f
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-02-14 00:37:13 -0400

    :construction: [CLEAN] Repository and [ADD] Starter interface
    Also the base design.ui file is included

--- controller/src/modules/controllers/packages/light.py
+++
@@ -1,8 +0,0 @@
-class Light:
-    def turn_on(self):
-        # TODO: Implement Lights on
-        print("Lights ON")
-        
-    def turn_off(self):
-        # TODO: Implement Lights off
-        print("Lights OFF")
--- controller/src/modules/hands_up/__init__.py
+++
@@ -1,9 +0,0 @@
-from .response import Response
-
-class HandsUp(Response):
-    def __init__(self, algorithm="mediapipe"):
-        if algorithm == "mediapipe":
-            self.algorithm = "mediapipe"
-        else:
-            raise ValueError("Unknown algorithm")
-
