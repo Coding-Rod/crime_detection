936376ba97b63fd7ece71f0f362477f5a25a6748
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-03-04 11:46:28 -0400

    :sparkles: [ADD] Nodes

--- frontend/src/views/system/ActiveNodes.vue
+++ frontend/src/views/system/ActiveNodes.vue
@@ -17,7 +17,7 @@
               <CCol md="3" xs="12">
                 <p class="text mb-3">Node Name: {{ node.name }}</p>
                 <p class="text mb-3">Node location: {{ node.location }}</p>
-                <p class="text mb-3">Node status: {{ node.status }}</p>
+                <p class="text mb-3">Node status: {{ node.status ? "Active 🟢" : "Inactive 🔴" }}</p>
               </CCol>
               <CCol md="9" xs="12">
                 <img :src="node.video" alt="node image" class="img-fluid" />
@@ -55,22 +55,7 @@ export default {
   data() {
     return {
       screenwidth: window.innerWidth,
-      nodes : [
-        {
-          name: "Node 1",
-          location: "Location 1",
-          status: "Active",
-          video: "https://picsum.photos/300/200",
-          id: 1,
-        },
-        {
-          name: "Node 2",
-          location: "Location 2",
-          status: "Active",
-          video: "https://picsum.photos/300/200",
-          id: 2,
-        },
-      ],
+      nodes : [],
       error: null,
     };
   },
@@ -82,15 +67,23 @@ export default {
       this.screenwidth = window.innerWidth;
     },
   },
-  mounted() {
+  async mounted() {
     window.addEventListener("resize", this.handleResize);
-    axios
-      .get("http://127.0.0.1:3000/api/v1/nodes")
-      .then((response) => {
-        this.nodes = response.data;
-      })
-      .catch((error) => {
-      });
+    console.log(`${this.$store.state.API_URL}/nodes/`);
+    try {
+      const response = await axios.get(
+        `${this.$store.state.API_URL}/nodes/`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      console.log(response.data);
+      this.nodes = response.data;
+    } catch (error) {
+      this.error = error;
+    }
   },
   beforeMount() {
     verifyToken();
