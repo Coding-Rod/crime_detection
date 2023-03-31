bf29f8e56c6ddb6dc38871de1b5f0a672489a491
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-03-30 23:51:01 -0400

    :wastebasket: [CLEAN] Other branches

--- backend/src/utils/swagger.ts
+++
@@ -1,6 +0,0 @@
-const swaggerAutogen = require("swagger-autogen")();
-
-const outputFile = "./src/assets/swagger.json";
-const endpointsFiles = ["./src/routes/index.ts"];
-
-swaggerAutogen(outputFile, endpointsFiles);
