51974a7067947e8b7ab22ddce2a92c253d5760d2
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-04-01 06:16:20 -0400

    :rocket: [DEPLOY] Backend

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -10,7 +10,6 @@ const config = {
     dbHost: process.env.DB_HOST,
     dbName: process.env.DB_NAME,
     dbPort: process.env.DB_PORT,
-    apiKey: process.env.API_KEY,
     jwtSecret: process.env.JWT_SECRET,
 };
 
