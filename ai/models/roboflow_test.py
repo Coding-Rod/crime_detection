1f90dc1369aad303c2d5141261a621e1f600c49c
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-04-15 16:26:37 -0400

    :bento: [UPDATE] sample image and model version

--- ai/models/roboflow_test.py
+++ ai/models/roboflow_test.py
@@ -2,10 +2,10 @@ from roboflow import Roboflow
 
 rf = Roboflow(api_key="wSbYaczzLzELee2lx4ed")
 project = rf.workspace("crime-detection-zbmr9").project("knifes-and-guns")
-model = project.version(4).model
+model = project.version(6).model
 
 # infer on a local image
-print(model.predict("/home/rodri/Downloads/images.jpeg", confidence=40, overlap=30).json())
+print(model.predict("sample.jpg", confidence=40, overlap=30).json())
 
 # visualize your prediction
 # model.predict("your_image.jpg", confidence=40, overlap=30).save("prediction.jpg")
